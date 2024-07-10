701dfcf10e99be4107b541023831d60787157121
CD Varias <variascieldean@gmail.com>
2024-07-10 14:10:03 +0800

    Testing transitions for the carousel

--- src/index.js
+++ src/index.js
@@ -5,6 +5,69 @@ const modal = document.getElementById("modal-id");
 const modalBtns = document.querySelectorAll("#modal-thumbnail-id");
 const closeBtns = document.querySelectorAll(".close");
 
+// selects all elements with className "mySlides" and place it inside the variable "slides"
+const slides = document.getElementsByClassName("mySlides"); 
+// selects the first element that matches the prev selector
+const prevButton = document.querySelector(".prev");
+// selects the first element that matches the next selector
+const nextButton = document.querySelector(".next");
+// selects the first element that matches the counter selector
+const counter = document.querySelector(".counter");
+
+// initializing the slide index to 1 (indicates the first slide)
+let slideIndex = 1;
+
+// calling showSlide function to display the first slide
+showSlides(slideIndex);
+
+// function plusSlides to increase or decrease the slide index by "n" and show the corresponding slide
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+//  function currentSlide to set the slide index to "n" and show the corresponding slide
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+//  function updateCounter to update the counter text to display the current slide number and total slides.
+function updateCounter(){
+    counter.textContent = `${slideIndex} / ${slides.length}`
+}
+
+// function showSlides to display the slide corresponding to the current slide index "n"
+function showSlides(n){
+    // variable i 
+    let i;
+    // if statement with condition that if the slideIndex is greater than the number of slides, set it to 1 (loop back to the first slide)
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    // if statement that if the slide index is less than 1, set it to the number of slides (loop back to the last slide)
+    if(n < 1){
+        slideIndex = slides.length
+    }
+    // for loop through all slides and set their display style to "none" (hide them)
+    for(i=0; i < slides.length; i+=1){
+        // slides that retrieves the number from an array, and put it style display to none
+        slides[i].classList.remove("active");
+    }
+    // display the slide corresponding to the current slide index
+    slides[slideIndex-1].classList.add("active");
+    // update the counter to reflect the current slide
+    updateCounter();
+}
+
+// Add an event listener to the prevButton to call plusSlides with -1 (go to the previous slide) when clicked
+prevButton.addEventListener("click", () =>{
+    plusSlides(-1);
+})
+
+// Add an event listener to the nextButton to call plusSlides with 1 (go to the next slide when clicked)
+nextButton.addEventListener("click", () =>{
+    plusSlides(1);
+})
+
 
 function goTop(){
     rootElement.scrollTo({
@@ -29,6 +92,7 @@ function handleScroll(){
 function openModal(){
     // show modal
     modal.style.display = "block";
+    
 }
 
 function closeModal(){
@@ -42,6 +106,7 @@ function outsideModal(event){
         modal.style.display = "none";
     }
 }
+
 
 
 document.addEventListener("scroll", handleScroll)
