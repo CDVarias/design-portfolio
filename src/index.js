1237fcbcc06dcd213d444c945fcf87be2b24f7ef
CD Varias <variascieldean@gmail.com>
2024-07-29 16:21:41 +0800

    3 Projects done w/ disclaimer

--- src/index.js
+++ src/index.js
@@ -1,73 +1,48 @@
 const goTopBtn = document.getElementById("gotopbtn-id");
 const rootElement = document.documentElement;
 
-const modal = document.getElementById("modal-id");
-const modalBtns = document.querySelectorAll("#modal-thumbnail-id");
+// const modal = document.getElementById("modal-id");
+const modalBtns = document.querySelectorAll(".modal-thumbnail");
 const closeBtns = document.querySelectorAll(".close");
 
-// selects all elements with className "mySlides" and place it inside the variable "slides"
-const slides = document.getElementsByClassName("mySlides"); 
-// selects the first element that matches the prev selector
-const prevButton = document.querySelector(".prev");
-// selects the first element that matches the next selector
-const nextButton = document.querySelector(".next");
-// selects the first element that matches the counter selector
-const counter = document.querySelector(".counter");
+const navbar = document.querySelector("header");
 
-// initializing the slide index to 1 (indicates the first slide)
-let slideIndex = 1;
+function initCarousel(carouselElement,slideClass){
+  const slides = carouselElement.querySelectorAll(`.${slideClass}`);
+  const prevButton = carouselElement.querySelector(".prev");
+  const nextButton = carouselElement.querySelector(".next");
+  const counter = carouselElement.querySelector(".counter");
+  carouselElement.slideIndex = 1;
 
-// calling showSlide function to display the first slide
-showSlides(slideIndex);
-
-// function plusSlides to increase or decrease the slide index by "n" and show the corresponding slide
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-
-//  function currentSlide to set the slide index to "n" and show the corresponding slide
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-
-//  function updateCounter to update the counter text to display the current slide number and total slides.
-function updateCounter(){
-    counter.textContent = `${slideIndex} / ${slides.length}`
-}
-
-// function showSlides to display the slide corresponding to the current slide index "n"
-function showSlides(n){
-    // variable i 
-    let i;
-    // if statement with condition that if the slideIndex is greater than the number of slides, set it to 1 (loop back to the first slide)
-    if(n > slides.length){
-        slideIndex = 1
+  function showSlides(n){
+    if (n > slides.length){
+      carouselElement.slideIndex = 1;
     }
-    // if statement that if the slide index is less than 1, set it to the number of slides (loop back to the last slide)
-    if(n < 1){
-        slideIndex = slides.length
+    if (n < 1) {
+      carouselElement.slideIndex = slides.length;
     }
-    // for loop through all slides and set their display style to "none" (hide them)
-    for(i=0; i < slides.length; i+=1){
-        // slides that retrieves the number from an array, and put it style display to none
-        slides[i].classList.remove("active");
-    }
-    // display the slide corresponding to the current slide index
-    slides[slideIndex-1].classList.add("active");
-    // update the counter to reflect the current slide
+    Array.from(slides).forEach(slide =>{
+      slide.classList.remove("active");
+    })
+    slides[carouselElement.slideIndex - 1].classList.add("active");
     updateCounter();
+  }
+
+  function plusSlides (n){
+    showSlides(carouselElement.slideIndex += n);
+  }
+
+  function updateCounter(){
+      if(counter){
+        counter.textContent = `${carouselElement.slideIndex} / ${slides.length}`;
+      }
+  }
+
+  showSlides(carouselElement.slideIndex);
+
+  prevButton.addEventListener("click", () => plusSlides(-1));
+  nextButton.addEventListener("click", () => plusSlides(1));
 }
-
-// Add an event listener to the prevButton to call plusSlides with -1 (go to the previous slide) when clicked
-prevButton.addEventListener("click", () =>{
-    plusSlides(-1);
-})
-
-// Add an event listener to the nextButton to call plusSlides with 1 (go to the next slide when clicked)
-nextButton.addEventListener("click", () =>{
-    plusSlides(1);
-})
-
 
 function goTop(){
     rootElement.scrollTo({
@@ -89,41 +64,82 @@ function handleScroll(){
     }
 }
 
-function openModal(){
-    // show modal
+function resetSlides(carouselElement, slideClass){
+  const slides = carouselElement.querySelectorAll(`.${slideClass}`);
+  const counter = carouselElement.querySelector(".counter");
+  carouselElement.slideIndex = 1;
+
+  Array.from(slides).forEach(slide =>{
+    slide.classList.remove("active");
+  })
+  slides[0].classList.add("active");
+
+  if (counter){
+    counter.textContent = `${carouselElement.slideIndex} / ${slides.length}`;
+  }
+}
+
+function openModal(event) {
+    const modalId = event.currentTarget.getAttribute("data-modal");
+    const modal = document.getElementById(modalId);
     modal.style.display = "block";
-    
-}
+    goTopBtn.style.display = "none";
+    document.querySelector(".navbar").classList.add("navbar-hidden");
+    document.body.style.overflow = "hidden";
+    // Add event listener to close modal when clicking outside
+    window.addEventListener("click", outsideModal);
+  }
 
-function closeModal(){
+
+function closeModal(e){
     // for x button in the modal
+    const modal = e.currentTarget.closest(".modal")
     modal.style.display = "none";
+    goTopBtn.style.display = "";
+    document.querySelector(".navbar").classList.remove("navbar-hidden");
+    document.body.style.overflow = "";
+
+    const carouselElement = modal.querySelector(".modal-left");
+    const slideClass = carouselElement.getAttribute("data-slide-class");
+    resetSlides(carouselElement,slideClass);
 }
 
-function outsideModal(event){
-    // when users clicked outside the modal, the modal closes
-    if (event.target === modal){
+// Function to close the modal when clicking outside of it
+function outsideModal(e) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+      if (e.target === modal) {
         modal.style.display = "none";
-    }
-}
+        goTopBtn.style.display = "";
+        document.querySelector(".navbar").classList.remove("navbar-hidden");
+        document.body.style.overflow = "";
 
+        const carouselElement = modal.querySelector(".modal-left");
+        const slideClass = carouselElement.getAttribute("data-slide-class");
+        resetSlides(carouselElement,slideClass);
 
+        window.removeEventListener("click", outsideModal);
+      }
+    });
+  }
 
 document.addEventListener("scroll", handleScroll)
 goTopBtn.addEventListener("click", goTop)
 
-// Loop through each elements with the id modalBtn and attach a click event listener
-modalBtns.forEach(modalBtn => {
-    // Add a click event listener to the current button for open modal
+modalBtns.forEach(modalBtn =>{
     modalBtn.addEventListener("click", openModal)
 })
 
 // Iterate through all elements that have the class "closeBtn"
 closeBtns.forEach(closeBtn => {
     // For each close button, add an event listener that listens for the "click event" and then call the closeModal function
-    closeBtn.addEventListener("click", closeModal)
+    closeBtn.addEventListener("click", closeModal);
 })
 
-window.addEventListener("click", outsideModal)
+document.querySelectorAll(".modal-left").forEach(carouselElement =>{
+  const slideClass = carouselElement.getAttribute("data-slide-class")
+  initCarousel(carouselElement,slideClass);
+})
+
 
 
