6167eb89e737f27308b11fc7bf7302de303ab54c
CD Varias <variascieldean@gmail.com>
2025-02-03 22:42:08 +0800

    Asset Management, with HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "assets/[name][ext]",
+    // assetModuleFilename: "assets/[name][ext]",
     clean: true,
   },
   module: {
